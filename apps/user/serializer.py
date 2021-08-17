85d0b7efbdf6141abfc460a313cf37d194949cf5
siddhant792 <siddhantgupta792000@gmail.com>
2021-08-17 17:23:29 +0530

    Added Token Based Auth

--- apps/user/serializer.py
+++ apps/user/serializer.py
@@ -1,12 +1,22 @@
 from rest_framework import  serializers
-from .models import User
+from rest_framework.authtoken.models import Token
 
-# Register serializer
-class RegisterSerializer(serializers.ModelSerializer):
+from apps.user.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    token = serializers.SerializerMethodField()
+    
     class Meta:
         model = User
-        fields = ('email', 'password', 'first_name', 'last_name')
-        
+        fields = ['first_name','last_name','email','password','token']
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
+
+    def get_token(self, user):
+        return Token.objects.get_or_create(user = user)[0].key
+
     def create(self, validated_data):
         user = User(
             first_name = validated_data['first_name'],
@@ -16,14 +26,3 @@ class RegisterSerializer(serializers.ModelSerializer):
         user.set_password(validated_data['password'])
         user.save()
         return user
-
-# User serializer
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['first_name','last_name','email','password']
-        extra_kwargs = {
-            'password': {'write_only': True},
-        }
-
-        
--- poker/settings/base.py
+++ poker/settings/base.py
@@ -6,7 +6,7 @@ class Setting(Settings):
 
     SECRET_KEY = 'bf#3+((2_t_-h0nt4k#)5md1rl%8p$o^*!k0+1v_^rb*j36(6g'
 
-    DEBUG = False
+    DEBUG = True
 
     ALLOWED_HOSTS = []
 
@@ -18,6 +18,7 @@ class Setting(Settings):
         'django.contrib.messages',
         'django.contrib.staticfiles',
         'rest_framework',
+        'rest_framework.authtoken',
         'apps.user',
     ]
 
@@ -55,7 +56,7 @@ class Setting(Settings):
 
     REST_FRAMEWORK = {
         'DEFAULT_AUTHENTICATION_CLASSES': [
-            'rest_framework_simplejwt.authentication.JWTAuthentication',
+            'rest_framework.authentication.TokenAuthentication',
         ],
     }
 
