0a5d3caf38045940873edc70fa5fd4c3f919d29a
Rohit Jain <rohit.jain@joshtechnologygroup.com>
2021-08-18 12:43:45 +0530

    PR fixes

--- apps/user/app_config.py
+++ apps/user/app_config.py
@@ -1,7 +1,10 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_save
 
 class UserConfig(AppConfig):
     name = 'apps.user'
     def ready(self) -> None:
-        import apps.user.signals
+        from apps.user.signals import send_email_handler
+        from apps.user.models import User
+        post_save.connect(send_email_handler, sender=User)
         return super().ready()
--- apps/user/signals.py
+++ apps/user/signals.py
@@ -1,11 +1,6 @@
-from django.dispatch import receiver
-from django.db import models
-
-from apps.user.models import User
 from apps.user.tasks import send_email_task
 
 
-@receiver(models.signals.post_save, sender=User)
-def handler(sender, instance, created, **kwargs):
+def send_email_handler(sender, instance, created, **kwargs):
     if created:
         send_email_task.delay(instance.email, instance.first_name)
