8298f692f10a789a70ba955036117a4b1621c16a
Rohit Jain <rohit.jain@joshtechnologygroup.com>
2021-08-20 12:45:36 +0530

    Removed unused task, fixed import order

--- poker/celery.py
+++ poker/celery.py
@@ -1,8 +1,8 @@
+import class_settings
 import os
-from django.conf import settings
 
 from celery import Celery
-import class_settings
+from django.conf import settings
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'poker.settings.local')
@@ -19,9 +19,4 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
-
-
-@app.task(bind=True)
-def debug_task(self):
-    print(self.request)
 
