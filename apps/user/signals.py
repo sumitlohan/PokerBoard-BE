df6f1b42496a405390ecc25c53d3c5fcdec81771
Rohit Jain <rohit.jain@joshtechnologygroup.com>
2021-08-21 14:37:06 +0530

    docstrings

--- apps/user/signals.py
+++ apps/user/signals.py
@@ -2,6 +2,9 @@ from apps.user.tasks import send_email_task
 
 
 def send_email_handler(**kwargs):
+    """
+    Django signal handler for sending email whenever a user is created
+    """
     instance = kwargs.get('instance')
     created = kwargs.get('created')
     if created:
