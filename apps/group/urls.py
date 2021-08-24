90e5f75d3157bf8498d97afe23c29e40631817eb
Rohit Jain <rohit.jain@joshtechnologygroup.com>
2021-08-24 12:58:20 +0530

    remove comments

--- apps/group/urls.py
+++ apps/group/urls.py
@@ -7,12 +7,5 @@ from apps.group.views import GroupApi, GroupUserApi
 router = routers.SimpleRouter(trailing_slash=False)
 router.register(r'', GroupApi, basename="groups")
 urlpatterns = [
-      # path('', GroupApi.as_view({
-      #       'get': 'list',
-      #       'post': 'create'
-      # })),
-      # path('<int:pk>', GroupApi.as_view({
-      #       'get': 'retrieve'
-      # })),
       path('create-members', GroupUserApi.as_view())
 ] + router.urls
