0adc2516da0b418dc18c36dc237262a0ca752a66
siddhant792 <siddhantgupta792000@gmail.com>
2021-08-26 17:18:35 +0530

    Fixes

--- apps/user/constants.py
+++ apps/user/constants.py
@@ -1,5 +1,4 @@
 PASSWORD_VALIDATION_MESSAGE = 'Password must be of minimum 8 characters, at least one uppercase letter, lowercase letter, number and special character'
 PASSWORD_VALIDATION_REGEX = '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$'
 EMAIL_VALIDATION_ERROR = 'This email activation link is invalid'
-EMAIL_VALIDATION_SUCCESS = 'Account Activated Successfully'
 EMAIL_REGISTER_SUBJECT = 'Verify Email Address for Pokerboard'
