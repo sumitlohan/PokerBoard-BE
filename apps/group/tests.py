3c22f2542deebf8ce0e5ebdfe7825eeaac2ffcf4
Rohit Jain <rohit.jain@joshtechnologygroup.com>
2021-09-07 10:58:18 +0530

    imports

--- apps/group/tests.py
+++ apps/group/tests.py
@@ -4,8 +4,8 @@ from django.urls import reverse
 
 from rest_framework.test import APITestCase
 
-import apps.group.models as group_models
-import apps.user.models as user_models
+from apps.group import models as group_models
+from apps.user import models as user_models
 
 
 class GroupTestCases(APITestCase):
