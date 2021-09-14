fb16aeddfa055258ef4d13d85b914ce4dc7cdb4f
Prabhcharan <prabhcharan.sethi@joshtechnologygroup.com>
2021-09-14 19:47:11 +0530

    Review Fixes

--- apps/invite/urls.py
+++ apps/invite/urls.py
@@ -7,11 +7,4 @@ import apps.invite.views as invite_views
 router = routers.SimpleRouter(trailing_slash=False)
 router.register(r'', invite_views.PokerboardMembersApi, basename="members")
 
-# urlpatterns = [
-#       path('invite', invite_views.InviteUserApi.as_view(), name='invite'),
-#       path('invite/<int:pk>', invite_views.AcceptInviteApi.as_view(), name='accept-invite'),
-#       path('remove/<int:pk>', invite_views.RemoveInviteeApi.as_view(), name='remove-invitee'),
-#       path('<int:pk>', invite_views.PokerboardMembersApi.as_view(), name='members')
-# ]
-
 urlpatterns = router.urls
